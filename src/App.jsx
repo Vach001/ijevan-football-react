16febdc01d13afa0f0f81d601a8cf66275636839
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-06-18 12:27:52 +0400

    feat(TableHeaderInfo): add new component

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,22 @@ import { Component } from "react";
 import "./App.css";
 import Table from "./components/Table/Table";
 import Club from "./components/Club/Club";
+import TableHeaderInfo from "./components/data-display/TableHeaderInfo/TableHeaderInfo";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.props = props;
-
-    this.state = {
-      isOpen: false,
-      btnText: "Open",
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    console.log("App component did mount");
-  }
-
-  componentDidUpdate() {
-    console.log("App component did update");
-  }
-
-  componentWillUnmount() {
-    console.log("App component will unmount");
-  }
-
-  handleClick() {
-    this.setState({
-      isOpen: !this.state.isOpen,
-      btnText: this.state.isOpen ? "Open" : "Close",
-    });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.isOpen ? <Club /> : <Table />}
-
-        <button onClick={this.handleClick}>{this.state.btnText}</button>
+        <TableHeaderInfo
+          title="Premier League"
+          description="Season 2020-2021"
+        />
       </div>
     );
   }
