00c3b13766c23c44a06dc9b45790e261cb2e93c8
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-06-22 09:56:42 +0400

    feat(Table): add table style and tableliner

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,54 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Table from "./components/Table/Table";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [state, setState] = useState("hello"); //  ['hello', () => {}]
+  const [counter, setCounter] = useState(0); // tuple -> kortej
 
-    this.props = props;
-  }
+  const handleHello = () => {
+    setState((prevState) => {
+      return prevState === "hello" ? "bye" : "hello";
+    });
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <Table />
-      </div>
-    );
-  }
+  useEffect(() => {
+    console.log("did mount");
+
+    return () => {
+      console.log("will unmount");
+    };
+  }, [counter]);
+
+  const handleClick = (number) => setCounter((prev) => prev + number);
+  const reset = () => setCounter(0);
+
+  return (
+    <div className="App">
+      {state === "hello" && <Table />}
+      <h1>{state}</h1>
+      <button onClick={handleHello}>Click</button>
+
+      <p>{counter}</p>
+      <button onClick={() => handleClick(1)}>Increment</button>
+      <button onClick={() => handleClick(-1)}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
 }
 
 export default App;
+
+// function foo(initialState) {
+//   let state = initialState
+
+//   function bla(value) {
+//     state = value
+//   }
+
+//   return [state, bla]
+// }
+
+// const [valod, samson] = foo('hello') // ['hello', bla]
+
+// console.log(valod, samson()) // 'hello'
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,41 +1,27 @@
-import { Component } from "react";
-
 import TableLiner from "../data-display/TableLiner/TableLiner";
 import TableHeader from "../data-display/TableHeader/TableHeader";
 
 import styles from "./Table.module.css";
+import { useEffect } from "react";
 
-class Table extends Component {
-  constructor(props) {
-    super(props);
+function Table({ text }) {
+  useEffect(() => {
+    console.log("table did mount");
 
-    this.text = props.text;
-  }
-
-  componentDidMount() {
-    console.log("Table component did mount");
-  }
-
-  componentDidUpdate() {
-    console.log("Table component did update");
-  }
-
-  componentWillUnmount() {
-    console.log("Table component will unmount");
-  }
-
-  render() {
-    return (
-      <section className={styles.table}>
-        <TableHeader
-          src="https://banner2.cleanpng.com/20180711/vg/kisspng-201617-premier-league-english-football-league-l-lion-emoji-5b460f06eeac18.5589169115313180229776.jpg"
-          title="Premier League"
-          description="Season 2020-2021"
-        />
-        <TableLiner />
-      </section>
-    );
-  }
+    return () => {
+      console.log("table will unmount");
+    };
+  }, []);
+  return (
+    <section className={styles.table}>
+      <TableHeader
+        src="https://banner2.cleanpng.com/20180711/vg/kisspng-201617-premier-league-english-football-league-l-lion-emoji-5b460f06eeac18.5589169115313180229776.jpg"
+        title="Premier League"
+        description="Season 2020-2021"
+      />
+      <TableLiner />
+    </section>
+  );
 }
 
 export default Table;
